c99ff90ac41f9a75c541d0fb448e66a6fbd55fad
Jonas Bordewick <bordewickjonas@gmail.com>
2023-06-05 17:26:13 +0200

    [ADD] Client Code

--- service/client.go
+++ service/client.go
@@ -2,14 +2,35 @@ package service
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/JonasBordewick/loadscheduler-log-service/database"
 	"github.com/JonasBordewick/loadscheduler-log-service/service/api"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
 type LogClient struct {
 	client  api.LogServiceClient
 	context context.Context
+}
+var instance *LogClient
+
+func GetClient(address string, port string) (*LogClient, error) {
+	if instance == nil {
+		connection, err := grpc.Dial(fmt.Sprintf("%s:%s", address, port), grpc.WithTransportCredentials(insecure.NewCredentials()))
+		if err != nil {
+			return nil, err
+		}
+		client := api.NewLogServiceClient(connection)
+		context := context.Background()
+
+		instance = &LogClient{
+			client: client,
+			context: context,
+		}
+	}
+	return instance, nil
 }
 
 func (client *LogClient) GetAllLogs() ([]*database.Log, error) {
